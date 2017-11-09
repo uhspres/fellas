1d6d4e20d8452a1bd658e48714e1d9a6bf949cb9
Goose1234 <32494827+Goose1234@users.noreply.github.com>
2017-11-09 13:47:36 -0500

    Title Screen

--- src/TitleScreen.py
+++ src/TitleScreen.py
@@ -2,14 +2,26 @@ import pygame
 from GameState import GameState
 
 class TitleScreen(GameState):
-	def __init__(self, stateHandler):
-		self.stateHandler = stateHandler
-	def update(self):
-		logo = pygame.image.load("/Users/Goose/Documents/GitHub/fellas/res/img/Fellas1.png")
-		white = (255, 255, 255)
-		self.screen.fill(white)
-		self.screen.blit(logo, (200, 25))
-		pygame.draw.rect(self.screen, (100, 100, 200), (250, 200, 300, 90))
-		pygame.draw.rect(self.screen, (100, 100, 200), (250, 350, 300, 90))
-		pygame.draw.rect(self.screen, (100, 100, 200), (250, 500, 300, 90))
-		pygame.display.flip()
+    def __init__(self, stateHandler):
+        self.stateHandler = stateHandler
+        self.screen = pygame.display.set_mode((800,600))
+    def update(self):
+        x = 250;
+        logo = pygame.image.load("../res/img/Fellas1.png")
+        logo = pygame.transform.scale(logo, (350, 130))
+        playButton = pygame.image.load("../res/img/playButton.png")
+        playButtonHover = pygame.image.load("../res/img/playButtonHover.png")
+        exitButton = pygame.image.load("../res/img/exitButton.png")
+        exitButtonHover = pygame.image.load("../res/img/exitButtonHover.png")
+        white = (255, 255, 255)
+        self.screen.fill(white)
+        self.screen.blit(logo, (225, 25))
+        self.screen.blit(playButton, (x, 100))
+        self.screen.blit(exitButton, (x, 200))
+        mosx, mosy = pygame.mouse.get_pos();
+        print(mosx, mosy)
+        if mosx > 260 and mosx < 500 and mosy > 190 and mosy < 266:
+            self.screen.blit(playButtonHover, (x, 100))
+        if mosx > 260 and mosx < 500 and mosy > 290 and mosy < 366:
+            self.screen.blit(exitButtonHover, (x, 200))
+        pygame.display.flip()
