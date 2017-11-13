1a0fd4f50b159c4e47c527bcdbe2b9cbcd4c238f
Goose1234 <32494827+Goose1234@users.noreply.github.com>
2017-11-13 13:53:59 -0500

    Fixed jump numbers

--- src/Player.py
+++ src/Player.py
@@ -4,6 +4,7 @@ from Character import Character
 class Player(Character):
     def __init__(self, image):
         super().__init__(140, 225, image)
+        parab = 0;
 
     def update(self):
         if self.jumping:
@@ -15,14 +16,14 @@ class Player(Character):
                 self.force = -(0.5 * self.mass * pow(self.velocity, 2)) # negative force for going down
 
             self.rect.y -= self.force # add force to the y position
-            parab = abs(12.9/(self.rect.y + .001))
+            parab = abs(80/(self.rect.y + .001))
 
-            self.velocity -= parab * parab + parab + .01; # reduce the velocity so that you fall eventually
+            self.velocity -= 2 * pow(parab, 2) + parab + .01; # reduce the velocity so that you fall eventually
             # reset physics
             if self.rect.y > 350: # check if player is on the ground
                 self.rect.y = 350
                 self.jumping = False
-                self.velocity = 6.5
+                self.velocity = 8
         
     def punch(self):
         print("Punch!")
