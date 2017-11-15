b0402ae0122289d6a4c5fd1d2996a4cc4e7cee7a
Ben <ben.greenberg@sixgables.com>
2017-11-14 22:12:53 -0500

    add cutscene system (framework) -- includes specification for scripting language.

--- src/Main.py
+++ src/Main.py
@@ -17,9 +17,6 @@ winSize = (800,600) # window size
 screen = pygame.display.set_mode(winSize)
 pygame.display.set_caption("Fellas... the game")
 
-#sprites.add(testCharacter)
-#sprites.add(testEnemy)
-
 # variables for the game loop
 running = True
 clock = pygame.time.Clock()
